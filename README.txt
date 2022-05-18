e4b7591269723521d115c7b1a43b50245b849045
andreaskon51 <50704825+andreaskon51@users.noreply.github.com>
2022-05-18 18:17:22 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,33 @@
+ █████  ███    ██ ██████  ██████  ███████  █████  ███████ ██   ██  ██████  ███    ██ ███████  ██ 
+██   ██ ████   ██ ██   ██ ██   ██ ██      ██   ██ ██      ██  ██  ██    ██ ████   ██ ██      ███ 
+███████ ██ ██  ██ ██   ██ ██████  █████   ███████ ███████ █████   ██    ██ ██ ██  ██ ███████  ██ 
+██   ██ ██  ██ ██ ██   ██ ██   ██ ██      ██   ██      ██ ██  ██  ██    ██ ██  ██ ██      ██  ██ 
+██   ██ ██   ████ ██████  ██   ██ ███████ ██   ██ ███████ ██   ██  ██████  ██   ████ ███████  ██ 
+                                                                                                 
+                                                                                                 
+================================= Simple Secure ACL Website ====================================
+
+Programming languges
+	- HTML
+	- CSS
+	- PHP
+
+DataBase:
+	- PhPMyAdmin
+
+
+How to setup the website:
+	- Install XAAMP
+	- Import the files into httpd folder
+	- Open PhPMyAdmin and import the loginpage.sql
+	- You are ready to go 
+
+How to access admin page:
+	- Change the link directory to /admin.php 
+	Admin Creadentials:
+		- username: admin
+		- password: admin
+
+How to access as a user:
+	- There is no signup file. The signup file is the index.php and there you will create the account
+	- you cant access the home.php without an account. Even if you try to change the directory.
---
+++ home.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<style>
+body,h1 {font-family: "Raleway", sans-serif}
+body, html {height: 100%}
+.bgimg {
+  background-image: url('photo.jpg');
+  min-height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+</style>
+</head>
+<body>
+<div class="w3-top">
+  <div class="w3-bar w3-black w3-card">
+    <a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+    <a href="#" class="w3-bar-item w3-button w3-padding-large">This is the user page</a>
+    <a href="logout.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Logout</a>
+      </div>
+    </div>
+<div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
+  <div class="w3-display-middle">
+    <h1 class="w3-jumbo w3-animate-top">Welcome</h1>
+    <hr class="w3-border-grey" style="margin:auto;width:40%">
+  </div>
+</div>
+
+</body>
+</html>
